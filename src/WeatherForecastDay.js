519ade28ae638e3526e80ab7263a98e07cd84c27
leirevalencia <132055494+leirevalencia@users.noreply.github.com>
2023-11-02 12:07:25 +0100

    update weatherforecastday

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,22 +2,39 @@ import React from "react";
 import './DailyForecast.css';
 
 export default function WeatherForecastDay(props) {
-    
+    function maxTemperature(){
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}º`;
+    }
+
+    function minTemperature(){
+        let temperature = Math.round(props.data.temperature.minimum);
+        return `${temperature}º`;
+        }
+
+    function day() {
+        let date = new Date(props.data.time*1000);
+        let day = date.getDay();
+
+        let days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+        return days[day];
+    }
+
     return(
         <div className="days">
             <p className="day">
-                mon
+                {day()}
             </p>
             <img 
-                src={props.data[0].condition.icon_url}
+                src={props.data.condition.icon_url}
                 alt={props.data.description}
             />
             <p className="max-min">
                 <span ClassName="min-temperature">
-                    {Math.round(props.data[0].temperature.minimum)}º | 
+                    {minTemperature()} | 
                 </span>
                 <span className="max-temperature">
-                    {Math.round(props.data[0].temperature.maximum)}º
+                    {maxTemperature()}
                 </span>
             </p>
     </div>
